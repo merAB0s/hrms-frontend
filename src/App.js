66cfcf6f18b0f347a82ecafb87ec8761a6ce106b
Abdullah Aksoy <merab0aks0y@gmail.com>
2021-06-08 20:33:43 +0300

    deneme

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { Container } from 'semantic-ui-react';
 import './App.css';
+import Navi from './layouts/Navi.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Container>
+          <Navi></Navi>
+          
+        </Container>
     </div>
   );
 }
